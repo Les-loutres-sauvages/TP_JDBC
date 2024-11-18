9e8475494f2d21d92f0ccbce3a289230549b65be
SolareFlame <zeldac332@gmail.com>
2024-11-18 10:59:59 +0100

    ✨ setDBName + config added

--- src/activeRecord/DBConnection.java
+++ src/activeRecord/DBConnection.java
@@ -4,10 +4,26 @@ import java.util.ArrayList;
 
 public class DBConnection {
 
+    // variables de connection
+    String userName = "root";
+    String password = "root";
+    String serverName = "127.0.0.1";
+    //String portNumber = "3306";
+    String portNumber = "8889"; // Port par défaut sur MAMP
+    String tableName = "personne";
+
+    static String dbName = "testpersonne";
+
     public static void getConnection() {
         //TODO
     }
 
-
+    /**
+     * Change le nom de la base d'accès.
+     * @param db_name Nouveau nom de la base à laquelle on souhaite accéder
+     */
+    public static void setNomDB(String db_name) {
+        dbName = db_name;
+    }
 
 }
