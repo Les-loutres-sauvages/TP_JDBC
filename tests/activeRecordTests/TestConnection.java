9b4384bf280b647b4b838afaaaee86e74d0dcd4b
SolareFlame <zeldac332@gmail.com>
2024-11-18 11:34:29 +0100

    🧪 Updated tests (for connection and connection type)

--- tests/activeRecordTests/TestConnection.java
+++ tests/activeRecordTests/TestConnection.java
@@ -1,6 +1,26 @@
 package activeRecordTests;
 
+import activeRecord.DBConnection;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class TestConnection {
 
-    //TODO
+    @Test
+    public void test_connection() throws Exception {
+        Connection co1 = DBConnection.getConnection();
+        Connection co2 = DBConnection.getConnection();
+
+        assertSame(co1, co2, "Les deux objets devraient être la même instance");
+    }
+
+    @Test
+    public void test_connection_type() throws Exception {
+        Connection co1 = DBConnection.getConnection();
+
+        assertInstanceOf(java.sql.Connection.class, co1, "L'objet devrait être une instance de java.sql.Connection");
+    }
 }
+
