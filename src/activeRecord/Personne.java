475f8e44dd5b27ebd8b6520b77e50f71d81d784c
SolareFlame <zeldac332@gmail.com>
2024-11-18 11:49:31 +0100

    ➕ Added FindAll

--- src/activeRecord/Personne.java
+++ src/activeRecord/Personne.java
@@ -1,5 +1,8 @@
 package activeRecord;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Personne {
 
     private int id;
@@ -9,5 +12,33 @@ public class Personne {
         this.nom = n;
         this.prenom = p;
         this.id = -1;
+    }
+
+    public Personne(String n, String p, int id) {
+        this.nom = n;
+        this.prenom = p;
+        this.id = id;
+    }
+
+    public static ArrayList<Personne> FindAll() throws SQLException {
+        String request = "SELECT * FROM Personne;";
+
+        ArrayList<Personne> res = new ArrayList<>();
+
+        Connection co = DBConnection.getConnection();
+        PreparedStatement prep = co.prepareStatement(request);
+        prep.execute();
+
+        ResultSet rs = prep.getResultSet();
+        while (rs.next()) {
+            String nom = rs.getString("nom");
+            String prenom = rs.getString("prenom");
+            int id = rs.getInt("id");
+
+            Personne p = new Personne(nom, prenom, id);
+            res.add(p);
+        }
+
+        return res;
     }
 }
